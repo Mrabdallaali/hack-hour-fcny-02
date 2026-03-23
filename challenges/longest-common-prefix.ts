5bd6e55de24f60a3b41245be72ffbc20e5c082aa
Mrabdallaali <mrabdallaalii@gmail.com>
2026-03-23 18:52:14 -0400

    refactored longest-common-prefix

--- challenges/longest-common-prefix.ts
+++ challenges/longest-common-prefix.ts
@@ -64,4 +64,22 @@ const longestCommonPrefix = (strs: string[]): string => {
 
 };
 
+
+//Approach solution
+const longestCommonPrefix = (strs: string[]): string => {
+  let result = '';
+  // iterate through each character of the first string in strs
+  // note how the "optional chaining operator" (?.) allows us to safely access length, even if strs is empty!
+  for (let i = 0; i < strs[0]?.length; i++) {
+    // iterate through each string in the array
+    for (let str of strs) {
+      // if any string in the array doesn't contain the same character at index i as the initial string, immediately return
+      if (str[i] !== strs[0][i]) return result;
+    };
+    // if each string in the array contains this character, add it to the result
+    result += strs[0][i];
+  };
+  return result;
+};
+
 export { longestCommonPrefix };
